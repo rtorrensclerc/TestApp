13ac80dfb8e08b31d468c50ec69ed90b67346511
rtorrensclerc <rtorrens@dimacofi.cl>
2020-03-26 02:04:15 -0300

    Updates

---
+++ precache-manifest.6316a5aa54711b5ac95de587bc5b1dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e64fa7a4231233fa3894cf885697d28",
+    "url": "/TestApp/index.html"
+  },
+  {
+    "revision": "248811cc0f0f1b5e419e",
+    "url": "/TestApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39dd699386e3bf8aba7d",
+    "url": "/TestApp/static/js/2.bd6e02ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestApp/static/js/2.bd6e02ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248811cc0f0f1b5e419e",
+    "url": "/TestApp/static/js/main.de42dcb2.chunk.js"
+  },
+  {
+    "revision": "2dc2a13660418a3b8db9",
+    "url": "/TestApp/static/js/runtime-main.8bb1cd6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
